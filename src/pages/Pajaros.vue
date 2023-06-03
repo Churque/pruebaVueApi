69aada690ab7836741fcc157b1826008fe94bb00
Claudio Churque <82532431+Xarkican@users.noreply.github.com>
2023-06-02 21:46:06 -0400

    correcion link en review hacia perfil

--- src/pages/Pajaros.vue
+++ src/pages/Pajaros.vue
@@ -1,58 +1,60 @@
 <template>
-    <div >
-      <ul>
-        <div class="container" v-for="producto in productos" :key="producto.id">
-          <div class="card" style="width: 18rem;">
-           <div class="images" v-for="image in producto.images" :key="image">
-           <img width="200" height="200"  :src="image" alt="Producto" >
+  <div>
+    <ul>
+      <div class="container" v-for="producto in productos" :key="producto.id">
+        <div class="card" style="width: 18rem">
+          <div v-if="producto.images.length > 1" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item" v-for="(image, index) in producto.images" :key="index" :class="{ 'active': index === 0 }">
+                <img :src="image" class="d-block w-100" alt="Producto" />
+              </div>
+            </div>
+          </div>
+          <div v-else>
+            <div class="images">
+              <img :src="producto.images[0]" class="d-block w-100" alt="Producto" />
+            </div>
           </div>
           <CartaProducto :producto="producto" />
-           
-  <div class="btn-group d-flex justify-content-center">
-            <button class="btn btn-sm btn-outline-secondary"> <RouterLink :to="`/Reviews/${producto._id}`">Detalles</RouterLink></button>
-          </div>
+
+          <div class="btn-group d-flex justify-content-center">
+            <button class="btn btn-sm btn-outline-secondary">
+              <RouterLink :to="`/Reviews/${producto._id}`">Detalles</RouterLink>
+            </button>
           </div>
         </div>
-      </ul>
-    </div>
-
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  import CartaProducto from '../components/CartaProducto.vue';
-  
-  
-  export default {
-    
-    
+      </div>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from "axios";
+import CartaProducto from "../components/CartaProducto.vue";
+export default {
   components: {
     CartaProducto,
   },
-    data() {
-      return {
-        productos: [],
-      };
+  data() {
+    return {
+      productos: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get("http://54.163.208.73:8080/products")
+        .then((result) => {
+          this.productos = result.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    mounted() {
-      this.fetchData();
-    },
-    methods: {
-      fetchData() {
-        axios
-          .get('http://54.163.208.73:8080/products')
-          .then((result) => {
-            this.productos = result.data;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
 
-  <style >
-
-  </style>
-  
+<style></style>
--- src/pages/Reviews.vue
+++ src/pages/Reviews.vue
@@ -2,7 +2,6 @@
   <div>
     <ul>
       <div class="container" v-if="producto">
-        
         <CartaProducto :producto="producto" />
         <div
           id="carouselExampleSlidesOnly"
@@ -17,27 +16,33 @@
         </div>
       </div>
 
-
-
       <div class="container" v-for="review in reviews" :key="review._id">
         <a> {{ review._id }}</a>
-          <div class="card" style="width: 18rem;">  
-           <img width="200" height="200" :src="review.user.photo" alt="Producto" >
-  <div class="card-body">
-    <h5 class="card-title">Nombre Reviewer: {{ review.user.name }}</h5>
-    <p class="card-text">{{ review.review }}</p>
-  </div>
+        <div class="card" style="width: 18rem">
+          <img
+            width="200"
+            height="200"
+            :src="review.user.photo"
+            alt="Producto"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Nombre Reviewer: {{ review.user.name }}</h5>
+            <p class="card-text">{{ review.review }}</p>
+            <div class="d-flex custom-centered">
+          <div class="btn-group d-flex justify-content-center">
+            <router-link :to="`/perfilusuario/${review.user._id}`" class="btn btn-sm btn-outline-secondary"> Ver Habilidades</router-link>
           </div>
+        </div>
+          </div>
+        </div>
       </div>
-
-
     </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import CartaProducto from '../components/CartaProducto.vue';
+import CartaProducto from "../components/CartaProducto.vue";
 
 export default {
   components: {
