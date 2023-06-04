6abc99659e528266cf825db9ca16cffe625f1611
Javier Ortega Zepeda <j.ortega11@ufromail.cl>
2023-06-03 20:36:16 -0400

    correcion navBar y diseño

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,9 +6,6 @@
           <strong>MarketPlace</strong>
         </div>
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
     </div>
   </div>
 </template>
